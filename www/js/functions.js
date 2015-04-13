657f36b783846ede05dfb2a861231f8c168f5d38
Dzouari <rakotoarivelo.johary@gmail.com>
2015-04-13 22:48:47 +0200

    0.2

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,34 +1,117 @@
-/* Fonctions JS*/
 $(document).ready(function() {
-	$('#menu div').click(function() {
-		page = $(this).attr("label");
-		page = '#' + page;
-		afficher(page);
-		/*page = $('#lilien').attr("href");
-		$.ajax({
-			url: page,
-			cache:false,
-			success:function(html) {
-				afficher(html);
-			},
-			error:function(XMLHttpRequest,textStatus,errorThrown) {
-				alert(textStatus);
-			}
-		})
-		return false;*/
-	});
-});
-
-function afficher(data) {
 	
-	$('#conteneur').animate({
-		left:"-5000"
-	},300,function() { 
-		$('#conteneur').empty();
-		$('#conteneur').append($(data));
-		$('#conteneur').animate({
-			left:"0"
-		},300);
+	/*Si on clique sur un*/
+	$('#un').click(function() {
+		$('#poster').animate({
+			left:"2000px",
+		},200, function(){
+			$('#mine').animate({
+				left:"2000px",
+			},200, function() {
+				$('#top').animate({
+					left:"2000px",
+				},200, function() {
+					$('#news').animate({
+						left:"0px",
+					}).show();
+				}).hide(1000);
+			}).hide(1000);
+		}).hide(1000);
+		
+		$('#selecteur').animate({
+			left:"0%"
+		},500, function() {
+			$('#un').css('color', '#ffffff');
+			$('#deux').css('color', '#808080');
+			$('#trois').css('color', '#808080');
+			$('#quatre').css('color', '#808080');
+		})
 	})
 	
-}
+	
+	/*Si on clique sur deux*/
+	$('#deux').click(function() {
+		$('#news').animate({
+			left:"-2000px"
+		},200, function(){
+			$('#mine').animate({
+				left:"2000px"
+			},200, function() {
+				$('#poster').animate({
+					left:"2000px"
+				},200, function() {
+					$('#top').animate({
+						left:"0px"
+					}).show();
+				}).hide(1000);
+			}).hide(1000);
+		}).hide(1000);
+		
+		$('#selecteur').animate({
+			left:"25%"
+		},500, function() {
+			$('#un').css('color', '#808080');
+			$('#deux').css('color', '#ffffff');
+			$('#trois').css('color', '#808080');
+			$('#quatre').css('color', '#808080');
+		})
+	})
+	
+	
+	/*Si on clique sur trois*/
+	$('#trois').click(function() {
+		$('#news').animate({
+			left:"-2000px",
+		},200, function(){
+			$('#top').animate({
+				left:"-2000px",
+			},200, function() {
+				$('#poster').animate({
+					left:"2000px",
+				},200, function() {
+					$('#mine').animate({
+						left:"0px",
+					}).show();
+				}).hide(1000);
+			}).hide(1000);
+		}).hide(1000);
+		
+		$('#selecteur').animate({
+			left:"50%"
+		},500, function() {
+			$('#un').css('color', '#808080');
+			$('#deux').css('color', '#808080');
+			$('#trois').css('color', '#ffffff');
+			$('#quatre').css('color', '#808080');
+		})
+	})
+	
+	
+	/*Si on clique sur quatre*/
+	$('#quatre').click(function() {
+		$('#news').animate({
+			left:"-2000px",
+		},200, function(){
+			$('#top').animate({
+				left:"-2000px",
+			},200, function() {
+				$('#mine').animate({
+					left:"-2000px",
+				},200, function() {
+					$('#poster').animate({
+						left:"0px",
+					}).show();
+				}).hide(1000);
+			}).hide(1000);
+		}).hide(1000);
+		
+		$('#selecteur').animate({
+			left:"75%"
+		},500, function() {
+			$('#un').css('color', '#808080');
+			$('#deux').css('color', '#808080');
+			$('#trois').css('color', '#808080');
+			$('#quatre').css('color', '#ffffff');
+		})
+	})
+})
