66f47d2185ca20efbab4e8d8385d36f735c25949
Dzouari <rakotoarivelo.johary@gmail.com>
2015-04-14 17:06:08 +0200

    0.4

--- www/js/functions.js
+++ www/js/functions.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
 		
 		/*Envoi*/
 		$.post(url,{type:type,id:id}, function(data) {
-			if(data.etat == "OK") {
+			/*if(data.etat == "OK") {
 				var number = data.nombre;
 				/*for (var i = 1; i < nombre; iter++) {
 					$selecteur.append(data.whisp.i);
-				}*/
+				}
 				alert(number);
 			}
-			else {
+			else {*/
 				alert(data.rapport);
-			}
+			/*}*/
 		});
 	}
 	
